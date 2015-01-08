497031e48c1331fde6b09f7b8e0942c29c31d01b
Jake Davis <jake.davis5989@gmail.com>
2015-01-08 00:21:40 +0000

    Cleanup

--- project.clj
+++ project.clj
@@ -11,14 +11,8 @@
                  [org.clojure/algo.generic "0.1.2"]
                  [cheshire "5.3.1"]
                  [clj-http "1.0.1"]]
-  :repositories
-  [["releases" {:url "http://nexus.banksimple.com/content/repositories/releases"
-                :username :env/nexus_username
-                :password :env/nexus_password
-                :sign-releases false}]]
   :min-lein-version "2.0.0"
-  :codox {:output-dir "target/doc"
-          :src-dir-url "https://github.banksimple.com/ops/rabbitmq-clj/blob/master"}
+  :codox {:output-dir "target/doc"}
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:plugins [[codox "0.8.10"]
                              [lein-cloverage "1.0.2"]]}})
