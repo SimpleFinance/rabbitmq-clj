f529547b5f42916a82e4ef2443e4de4e5c5867ab
Jake Davis <jake.davis5989@gmail.com>
2015-01-09 23:52:41 +0000

    Add that test I said I added ...

--- test/rabbitmq_clj/core_test.clj
+++ test/rabbitmq_clj/core_test.clj
@@ -4,5 +4,7 @@
 
 (deftest core
   (testing "All endpoints"
-    (let [overview (dispatch :overview)]
-      (is (map? overview)))))
+    (let [overview (dispatch :overview)
+          nodes (dispatch :arbitrary :get "/api/nodes")]
+      (is (map? overview))
+      (is (coll? nodes)))))
